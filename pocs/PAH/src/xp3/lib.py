c23ef434085b41ea545b2a8eda69b756ea05c3a1
Damien Marchal <damien.marchal@univ-lille1.fr>
2025-03-20 13:05:05 +0100

    Add third experiment in which I explore the python behavior regarding class and annotations.

---
+++ pocs/PAH/src/xp3/lib.py
@@ -0,0 +1,111 @@
+import dataclasses
+import inspect
+import typing
+
+def is_object_description(cls : type) -> bool:
+    return issubclass(cls,SofaObject)
+
+def is_node_description(cls : type) -> bool:
+    return issubclass(cls,SofaNode)
+
+@dataclasses.dataclass
+class MemberInfo(object):
+    name : str
+    value : typing.Any
+    type : type
+
+    def __str__(self):
+        return (str(self.name), str(self.value), str(type))
+
+
+# Mock sofa classes . 
+class SofaBase:
+    name : str = "Unnamed"
+
+    @classmethod
+    def get_data_declared(cls) -> dict[str, MemberInfo]:
+        
+        selected_members = {}
+        print("RESULT ", cls.get_members_declared().values())
+        for info in cls.get_members_declared().values():
+            print("PROCESS, ",info.name)
+            print("TOTO => ", type(info), info.name, info.type)
+            print("POURUQOI" )
+            if issubclass(info.type, BaseData):
+                selected_members[info.name] = info
+        return selected_members
+
+    @classmethod
+    def get_members_declared(cls) -> dict[str, MemberInfo]:
+        attributes = {}
+        print(" => ", typing.get_type_hints(cls))
+
+        for name, value in typing.get_type_hints(cls).items():  
+            attributes[name] = MemberInfo(name, None, type=value)
+           
+        for name, value in inspect.getmembers(cls):  
+            if not name.startswith('_'):
+                if not inspect.ismethod(value) and not inspect.isfunction(value): 
+                    if name in attributes:
+                        attributes[name] = MemberInfo(name=name, value=value, type=type(None))
+                    else:
+                        attributes[name] = MemberInfo(name=name, value=value, type=attributes[name].type)
+        return attributes
+
+    @classmethod
+    def clone(cls):
+        """Deep clone of the cls, compare to simple duplication the initialized members are also copied"""
+        attributes = {} 
+        # Inspect static members 
+        for i in inspect.getmembers_static(cls):  
+            # to remove private and protected
+            # functions
+            if not i[0].startswith('_'):
+                # Remove methods: 
+                if not inspect.ismethod(i[1]): 
+                    attributes[i[0]] = i[1]
+
+        return type(cls.__name__+"Instance", (cls, ), attributes)
+
+class LinkPath:
+    pass 
+
+class BaseData:
+    pass
+
+# Here T is a generic type (if unused, just think of it as c++ template)
+class DataValue[T](BaseData):
+    value : T 
+    linkpath : str
+    
+    def setParent(self): pass  
+
+# Here T is a generic type (if unused, just think of it as c++ template)
+class DataVector[T](BaseData):
+    value : list[list[T]] 
+    linkpath : str
+    
+    def setParent(self): pass  
+
+class SofaNode(SofaBase):
+    bbox : DataValue[float] 
+    
+    def addNode():
+        return 
+
+    def addObject():
+        return 
+            
+class SofaObject(SofaBase):
+    pass 
+
+## Example of class declaration corresponding to MechanicalObject
+@dataclasses.dataclass
+class MechanicalObject(SofaObject):
+    position : DataVector[float]
+    rest_position : DataVector[float]
+
+    showObject : DataValue[bool]
+
+    def to_dict(self):
+        return dataclasses.asdict(self)
